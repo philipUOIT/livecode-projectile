75d5251a80541f88b037e6e013f10b9fd1cdc96d
Philip Dos santos <100625145@oncampus.local>
2017-09-25 13:01:10 -0400

    second commit, catching up to teacher

--- projectile.hpp
+++ projectile.hpp
@@ -2,18 +2,15 @@
 
 #include <cstdio>
 
-struct VecR2{
+struct VecR2 {
   double x{0};
   double y{0};
-
 };
 
-auto operator+(vecR2 a, VecR2 b){
-  return VecR2 {a.x + b.x, a.y + b.y};
-}
-
-auto operator+(VecR2 a, double c){return VecR2{c*a.x, c*a.y};}
-auto operator+(double c, VecR2 a){return a*c;}
+auto operator+(VecR2 a, VecR2 b) { return VecR2{a.x + b.x, a.y + b.y}; }
+auto operator*(VecR2 a, double c) { return VecR2{c * a.x, c * a.y}; }
+auto operator*(double c, VecR2 a) { return a * c; }
+auto operator/(VecR2 a, double c) { return VecR2{a.x / c, a.y / c}; }
 
 struct TState {
   double t{0};
@@ -25,4 +22,3 @@ void print_tstate(TState s) {
   printf("%f %f %f %f %f\n", s.t, s.position.x, s.position.y, s.velocity.x,
          s.velocity.y);
 }
-//ignore
